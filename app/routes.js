abbb724b0ae01aa2a397b71c4dbe2e337c54888f
Dongbin Kim <bindongkim@gmail.com>
2017-06-01 00:40:11 +0900

    Setup routes for each case

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,40 @@ var path = require('path');
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+    res.sendFile(path.join(__dirname, '../views', 'base.html'));
+  });
+
+  app.get('/case1', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case1.html'));
+  });
+
+  app.get('/case2', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case2.html'));
+  });
+
+  app.get('/case3', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case3.html'));
+  });
+
+  app.get('/case4', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case4.html'));
+  });
+
+  app.get('/case5', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case5.html'));
+  });
+
+  app.get('/case6', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'case6.html'));
   });
 
   app.post('/choice', function(req, res) {
-    var choice = req.body.choice;
-    var userChoice = new Choice();
-    userChoice.choice = choice;
+    var testCase = req.body.testCase;
+    var choice   = req.body.choice;
+
+    var userChoice      = new Choice();
+    userChoice.testCase = testCase;
+    userChoice.choice   = choice;
     userChoice.save().then(function() {
       res.send('Success');
     }, function(err) {
