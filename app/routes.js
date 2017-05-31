e1717fc5cc026e1b6fe45ffc54db1d34693186f5
Dongbin Kim <bindongkim@gmail.com>
2017-05-31 11:55:54 +0900

    Setup router

---
+++ app/routes.js
@@ -0,0 +1,21 @@
+var Choice = require('./models/choice.js');
+var path = require('path');
+
+module.exports = function(app) {
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  });
+
+  app.post('/choice', function(req, res) {
+    var choice; // user's choice
+    var userChoice = new Choice();
+    userChoice.choice = choice;
+    userChoice.save().then(function() {
+      res.send('Success');
+    }, function(err) {
+      console.log('Error: ' + err);
+      res.send('Error');
+    });
+  });
+};
