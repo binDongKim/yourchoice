53cad22b10883159af82cb668830d966d67530b4
Dongbin Kim <bindongkim@gmail.com>
2017-06-01 00:40:36 +0900

    Add testCase property

--- app/models/choice.js
+++ app/models/choice.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var choiceSchema = mongoose.Schema({
-  choice : String
+  testCase: String,
+  choice  : String
 });
 
 module.exports = mongoose.model('Choice', choiceSchema);
