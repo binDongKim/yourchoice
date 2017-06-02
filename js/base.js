c05d9154980c0b9ae5a71e11957561c67c86785b
Dongbin Kim <bindongkim@gmail.com>
2017-06-02 12:40:31 +0900

    Either (case1, case2, case3) or (case4, case5, case6) or case7

--- js/base.js
+++ js/base.js
@@ -1,35 +1,74 @@
 $(document).ready(function() {
-  var currentTestCase = $('main').data('case');
-  var userChoice = [];
-  manipulateResult(currentTestCase, 3500);
+  var testType = $('main').data('type'); // a, b, c
+
+  var aCases = ['case1', 'case2', 'case3'];
+  var bCases = ['case4', 'case5', 'case6'];
+  var cCase  = 'case7';
+
+  var currentCases;
+  if(testType === 'a') {
+    currentCases = aCases;
+  } else if(testType === 'b') {
+    currentCases = bCases;
+  }
+
+  var userCase = testType === 'c' ? cCase : popCase(currentCases);
+  console.log('첫번째: ' + userCase);
+  manipulateNumber(userCase, 3500);
+
   $('button').click(function() {
-    userChoice.push($(this).data('choice'));
+    var userChoice = $(this).data('choice');
     var currentSectionColor = $('section').data('color');
     switch (currentSectionColor) {
       case 'red':
-        greenBtns(currentTestCase);
-        manipulateResult(currentTestCase, 2500);
+        $.post('/choice', { userCase: userCase, choice: userChoice }).done(function(message) {
+          if(message === 'Success') {
+            greenBtns(testType);
+            userCase = testType === 'c' ? cCase : popCase(currentCases);
+            console.log('두번째: ' + userCase);
+            manipulateNumber(userCase, 2500);
+          } else {
+            $('main'.html('<p class="card-panel red white-text">에러가 발생하였습니다!!</p>'));
+          }
+        });
         break;
       case 'green':
-        blueBtns(currentTestCase);
-        manipulateResult(currentTestCase, 3000);
+        $.post('/choice', { userCase: userCase, choice: userChoice }).done(function(message) {
+          if(message === 'Success') {
+            blueBtns(testType);
+            userCase = testType === 'c' ? cCase : popCase(currentCases);
+            console.log('세번째: ' + userCase);
+            manipulateNumber(userCase, 2000);
+          } else {
+            $('main'.html('<p class="card-panel red white-text">에러가 발생하였습니다!!</p>'));
+          }
+        });
         break;
       case 'blue':
-        $.post('/choice', { testCase: currentTestCase, choice: userChoice }).done(function() {
-          $('main').html('<p class="thanks card-panel teal lighten-1 white-text">참여해주셔서 감사합니다.</p>');
+        $.post('/choice', { userCase: userCase, choice: userChoice }).done(function(message) {
+          if(message === 'Success') {
+            $('main').html('<p class="thanks card-panel teal lighten-1 white-text">참여해주셔서 감사합니다.</p>');
+          } else {
+            $('main'.html('<p class="card-panel red white-text">에러가 발생하였습니다!!</p>'));
+          }
         });
+        break;
     }
   });
 });
 
-function manipulateResult(testCase, msLater) {
+function popCase(caseArr) {
+  var index = Math.floor((Math.random() * caseArr.length)); // 0, 1, 2
+  return caseArr.splice(index,1).toString();
+}
+
+function manipulateNumber(testCase, msLater) {
   var caseNum = Number(testCase.slice(-1)); // 1,2,3,4,5,6,7
   /*
     1,4,7일땐 do nothing
     2,5일땐 첫번째 button value+1
     3,6일땐 두번째 button value+1
   */
-
   var index = (caseNum+1)%3;
   if(index !== 2) {
     var targetButton = $('button:eq(' + index + ')');
@@ -40,42 +79,30 @@ function manipulateResult(testCase, msLater) {
   }
 }
 
-function greenBtns(testCase) {
+function greenBtns(testType) {
   $('section').data('color', 'green');
   $('.choice-guide').html('두 버튼 중 <u>더 짙은 초록색</u> 버튼을 클릭해주세요.');
   $('button').removeClass('red').addClass('green');
-  switch (testCase) {
-    case 'case1':
-    case 'case2':
-    case 'case3':
-      $('button:eq(0)').text(91);
-      $('button:eq(1)').text(20);
-      break;
-    case 'case4':
-    case 'case5':
-    case 'case6':
-      $('button:eq(0)').text(50);
-      $('button:eq(1)').text(47);
-      break;
+
+  if(testType === 'a') {
+    $('button:eq(0)').text(91);
+    $('button:eq(1)').text(20);
+  } else if(testType === 'b') {
+    $('button:eq(0)').text(50);
+    $('button:eq(1)').text(47);
   }
 }
 
-function blueBtns(testCase) {
+function blueBtns(testType) {
   $('section').data('color', 'blue');
   $('.choice-guide').html('두 버튼 중 <u>더 파란</u> 버튼을 클릭해주세요.');
   $('button').removeClass('green').addClass('blue');
-  switch (testCase) {
-    case 'case1':
-    case 'case2':
-    case 'case3':
-      $('button:eq(0)').text(83);
-      $('button:eq(1)').text(28);
-      break;
-    case 'case4':
-    case 'case5':
-    case 'case6':
-      $('button:eq(0)').text(54);
-      $('button:eq(1)').text(43);
-      break;
+
+  if(testType === 'a') {
+    $('button:eq(0)').text(83);
+    $('button:eq(1)').text(28);
+  } else if(testType === 'b') {
+    $('button:eq(0)').text(54);
+    $('button:eq(1)').text(43);
   }
 }
